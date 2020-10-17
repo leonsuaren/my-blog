2119b714675a82d7985cccbec067ee05cde7830e
Leon Suarez <leonsua@gmail.com>
2020-10-16 20:37:02 -0400

    First commit

---
+++ src/build/precache-manifest.fe30cc3059714f68d9a6d8fddf6879b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2777450b2f6598ec3b460389da0ab969",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b147be4172872f865444",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "9a44c9ff75f0c5cb04d6",
+    "url": "/static/js/2.f7b65722.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f7b65722.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b147be4172872f865444",
+    "url": "/static/js/main.5e6c5218.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
